68e0fd427a59ff49be140399cb8fe770530db95f
nero8919 <74824730+nero8919@users.noreply.github.com>
2026-01-22 07:14:17 +0000

    Deploying to gh-pages from @ nero8919/nero8919.github.io@4185c2baf4e668e8b6bb8e1c4bf48049e857816c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -539,14 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=3P_iQXQAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
